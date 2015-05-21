7cc2c95a61fc66375865fd661ce1eaec9678cff3
ChanalTanguy <tanguy.chanal@gmail.com>
2015-05-21 14:37:55 +0200

    Ajout de la classe SettingsPanel Construit un panel d'options

---
+++ StreetCar/src/panelPackage/SettingsPanel.java
@@ -0,0 +1,15 @@
+package panelPackage;
+
+import javax.swing.JPanel;
+
+public class SettingsPanel extends JPanel {
+	
+	public SettingsPanel(){
+		this.buildSettingsPanel();
+	}
+	
+	private void buildSettingsPanel(){
+		
+	}
+	
+}
