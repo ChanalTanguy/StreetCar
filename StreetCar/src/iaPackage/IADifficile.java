2d69c60e37e76429fa24fc7062c53253c86e84a6
Piapiou <pierre-42@live.fr>
2015-05-21 09:55:28 +0200

    Légère correction dans setListeConnections

--- StreetCar/src/iaPackage/IADifficile.java
+++ StreetCar/src/iaPackage/IADifficile.java
@@ -1,16 +1,21 @@
 package iaPackage;
 
 import joueurPackage.Coup;
-import mainPackage.Moteur;
+import mainPackage.*;
 
 public class IADifficile implements InterfaceIA {
 	
+	Moteur moteur;
+	
 	public IADifficile(Moteur moteur) {
-		
+		this.moteur = moteur;
 	}
 
 	public Coup getCoup() {
-		// * Générer le graphe (ou pas selon commenjt on s'y prend
+		
+		Plateau plateau = (Plateau) moteur.getPlateau().clone();
+		
+		// * Générer le graphe (ou pas selon comment on s'y prend)
 		// ** Trouver le chemin le plus rapide pour rejoindre ses terminus
 		// *** Placer une tuile au hasard sur le chemin (avec Rotation potentiel)
 		// ** Deviner les objectifs de l'adversaire
