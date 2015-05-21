9e86126a9d186c3a1ae475ec90312764ded859b1
Piapiou <pierre-42@live.fr>
2015-05-21 11:12:42 +0200

    Ajout de l'initialisation Plateau. Modification de Escale et terminus

--- StreetCar/src/tuilePackage/Escale.java
+++ StreetCar/src/tuilePackage/Escale.java
@@ -5,11 +5,11 @@ public class Escale extends Tuile{
 	int numeroEscale;
 	
 	public Escale (){
-		super();
+		super(true);
 	}
 	
 	public Escale (int newNumero){
-		super();
+		super(true);
 		numeroEscale = newNumero;
 	}
 }
