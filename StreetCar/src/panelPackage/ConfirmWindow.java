f377ff302b15e9c06381fc4389c9598c8f496b7a
ChanalTanguy <tanguy.chanal@gmail.com>
2015-05-27 14:17:06 +0200

    Confirmation lors du clic sur "Quitter"

---
+++ StreetCar/src/panelPackage/ConfirmWindow.java
@@ -0,0 +1,83 @@
+package panelPackage;
+
+import java.awt.BorderLayout;
+import java.awt.Dialog;
+import java.awt.Dimension;
+import java.awt.FlowLayout;
+import java.awt.event.ActionListener;
+
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JDialog;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.SwingConstants;
+
+public class ConfirmWindow extends JDialog {
+	
+	ActionListener actionYES;
+	ActionListener actionNO;
+	
+	public ConfirmWindow(ActionListener actionYES, ActionListener actionNO){
+		this.actionYES = actionYES;
+		this.actionNO = actionNO;
+	}
+	
+	public void openConfirmWindow(){
+		this.setWindow();
+		this.setVisible(true);
+	}
+	
+	private void setWindow(){
+		this.setSize(275,125);
+		this.setResizable(false);
+		this.setLocationRelativeTo(null);
+		this.setTitle("Allan please add title");
+		
+		JPanel panel = buildConfirmPanel();
+		this.add(panel);
+		
+		this.setModal(true);
+		this.setDefaultCloseOperation(JDialog.DISPOSE_ON_CLOSE);
+		this.setModalExclusionType(Dialog.ModalExclusionType.APPLICATION_EXCLUDE);
+	}
+	
+	private JPanel buildConfirmPanel(){
+		JPanel confirm = new JPanel();
+		
+		JPanel zone1 = newZone(250, 40);
+		JPanel zone2 = newZone(200, 40);
+		
+		JLabel label = new JLabel("Êtes vous sûr ?", SwingConstants.CENTER);
+		
+		zone1.add(label);
+		addNewButton(zone2, "OUI", BorderLayout.WEST, actionYES, null);
+		addNewButton(zone2, "NON", BorderLayout.EAST, actionNO, null);
+		
+		confirm.add(zone1);
+		confirm.add(zone2);
+		
+		return confirm;
+	}
+	
+	private JPanel newZone(int x, int y){
+		JPanel panel = new JPanel();
+		panel.setLayout(new BorderLayout());
+		panel.setPreferredSize(new Dimension(x,y));
+		return panel;
+	}
+	
+	private void addNewButton(JPanel panel, String text, String position, ActionListener action, ImageIcon img){
+		Dimension size = new Dimension(85,40);
+		JButton button = new JButton(text, img);
+		button.setPreferredSize(size);
+		button.addActionListener(action);
+		panel.add(button, position);
+	}
+	
+	public void setListeners(ActionListener actionYES, ActionListener actionNO){
+		this.actionYES = actionYES;
+		this.actionNO = actionNO;
+	}
+}
--- StreetCar/src/panelPackage/PanelListener.java
+++ StreetCar/src/panelPackage/PanelListener.java
@@ -1,60 +1,72 @@
 package panelPackage;
 
+import java.awt.Dialog;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 import javax.swing.JDialog;
 import javax.swing.JFrame;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 
 public class PanelListener {
-	
+
 	public class ConfigureNewGameButtonListener implements ActionListener {
 
 		// Clic sur le bouton "Nouvelle partie" du menu principal
-		
+
 		public void actionPerformed(ActionEvent e) {
 			MenuWindow win = new MenuWindow();
 			win.openNewGameWindow();
 		}
-		
+
+	}
+
+	public class QuitButtonListener implements ActionListener {
+
+		public void actionPerformed(ActionEvent e) {
+			ConfirmWindow conf = new ConfirmWindow(null, null);
+			PanelListener listener = new PanelListener();
+			conf.setListeners(listener.new Quit(), listener.new ReturnButtonListener(conf));
+			conf.openConfirmWindow();
+		}
 	}
 	
-	public class QuitButtonListener implements ActionListener {
-		
+	public class Quit implements ActionListener {
+
 		public void actionPerformed(ActionEvent e) {
 			System.exit(0);
 		}
 		
 	}
-	
+
 	public class ReturnButtonListener implements ActionListener {
 
 		// Clic sur le bouton "Cancel" d'un des menus
-		
+
 		JDialog parentDialog;
 		JPanel parentPanel;
 		JFrame parentFrame;
 		JPanel panel;
-		
+
 		public ReturnButtonListener(JDialog parent){
 			parentDialog = parent;
 		}
-		
+
 		public ReturnButtonListener(JPanel panel, JPanel parent){
 			parentPanel = parent;
 		}
-		
+
 		public ReturnButtonListener(JFrame parent){
 			parentFrame = parent;
 		}
-		
+
 		public void actionPerformed(ActionEvent e) {
 			if(parentDialog != null){ parentDialog.dispose(); }
 			if(parentPanel != null){ parentPanel.remove(panel); }
 			if(parentFrame != null){ parentFrame.dispose(); }
 		}
-		
+
 	}
 
 }
