64f4adc70e4268993f413a26007af6b3510bbba3
vov <vov@127.0.0.1>
2015-06-04 13:47:15 +0200

    suppression d'attributs/methodes inutiles

--- StreetCar/src/graphique/Pan_Abstract.java
+++ StreetCar/src/graphique/Pan_Abstract.java
@@ -10,11 +10,9 @@ public abstract class Pan_Abstract extends JComponent{
 	protected int largeur, hauteur;
 	protected Graphics2D crayon;
 	protected Color couleur;
-	protected boolean contoursSurlignes;
 	
 	public Pan_Abstract (Color newCouleur){
 		couleur = newCouleur;
-		contoursSurlignes = false;
 		addMouseListener(new GestionSouris(this));
 	}
 	
@@ -23,15 +21,6 @@ public abstract class Pan_Abstract extends JComponent{
 	}
 	protected int getHauteur (){
 		return hauteur;
-	}
-	
-	protected void activerContours (){
-		contoursSurlignes = true;
-		repaint();
-	}
-	protected void desactiverContours (){
-		contoursSurlignes = false;
-		repaint();
 	}
 	
 	public abstract void paintComponent (Graphics g);
