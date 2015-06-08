f5a938e7c33d916668d17d371ef085867a613a44
vov <vov@127.0.0.1>
2015-06-08 12:13:24 +0200

    Ajout d'une constante de type de coup : AvanceeTram

--- StreetCar/src/constantesPackages/Constantes.java
+++ StreetCar/src/constantesPackages/Constantes.java
@@ -55,6 +55,7 @@ public class Constantes {
 		public static final String pioche = "Pioche";
 		public static final String vol = "Vol";
 		public static final String placement = "Placement";
+		public static final String avanceeTrame = "AvanceeTram";
 	}
 	
 	public static class Images {
