dd4482ef9937760315099b2eec7edbd62da16639
vov <vov@127.0.0.1>
2015-06-08 14:40:04 +0200

    MaJ EcouteurPlateau pour debuter le voyage inaugural

--- StreetCar/src/graphique/Ecouteur_Plateau.java
+++ StreetCar/src/graphique/Ecouteur_Plateau.java
@@ -10,53 +10,53 @@ import mainPackage.Moteur;
 public class Ecouteur_Plateau implements MouseListener, MouseMotionListener{
 	Panneau_Plateau panneauDeJeu;
 	Moteur mot;
-	
+
 	public Ecouteur_Plateau (Panneau_Plateau referencePan, Moteur referenceMoteur){
 		panneauDeJeu = referencePan;
 		mot = referenceMoteur;
 	}
-	
+
 	public void mousePressed(MouseEvent e) {
-		
+
 		if (mot.getTabPlayers()[mot.getcurrentPlayer()].getType() == 0){
 			// Joueur actif
 			JoueurHumain joueur = (JoueurHumain) mot.getTabPlayers()[mot.getcurrentPlayer()];
 			int buttonDown = e.getButton();
-			
+
 			// Coordonnees
 			int x = e.getX();
 			int y = e.getY();
-			
+
 			// on supprime le decalage de dessin par rapport a la bordure gauche.
 			int piocheX = x - panneauDeJeu.getDepart();
 			int piocheY = y;
-			
+
 			int caseX = (x - panneauDeJeu.getDepart() - 8) / panneauDeJeu.getTailleCase();
 			int caseY = (y - panneauDeJeu.getDepart() - 10) / panneauDeJeu.getTailleCase();
-			
+
 			switch (buttonDown){
 			// bouton droit de la souris
 			case MouseEvent.BUTTON3:
 				if ( estDansMain(piocheX, piocheY) && (mot.getcurrentPlayer() == mainNo(piocheY) - 1) 
-					&& joueur.getMain().getTuileAt(tuileNo(piocheX)) != null
-					&&  (panneauDeJeu.getCoupSimultaneActif() == null 
-					|| panneauDeJeu.getCoupSimultaneActif() != null && panneauDeJeu.getCoupSimultaneActif().getTuile() != tuileNo(piocheX)) ){
-					
+						&& joueur.getMain().getTuileAt(tuileNo(piocheX)) != null
+						&&  (panneauDeJeu.getCoupSimultaneActif() == null 
+						|| panneauDeJeu.getCoupSimultaneActif() != null && panneauDeJeu.getCoupSimultaneActif().getTuile() != tuileNo(piocheX)) ){
+
 					panneauDeJeu.setPiocheSelectionnee(false);
 					panneauDeJeu.setCaseSelectionnee(false);
 					int numeroTuile = tuileNo(piocheX);
 					int numeroMain = mainNo(piocheY);
 					joueur.coupTourner(numeroTuile);
-					
+
 					selectionnerMain(numeroTuile, numeroMain);
 					//selectionnerPioche();
 				}
 				break;
-			// bouton gauche de la souris
+				// bouton gauche de la souris
 			case MouseEvent.BUTTON1:
 				// si le joueur tente de cliquer
 				if ( estSurPioche(x, y) ){
-					
+
 					panneauDeJeu.setCaseSelectionnee(false);
 					panneauDeJeu.setMainSelectionnee(-1);
 					joueur.coupPiocher();
@@ -64,12 +64,12 @@ public class Ecouteur_Plateau implements MouseListener, MouseMotionListener{
 					panneauDeJeu.setVisObjJ2(false);
 					panneauDeJeu.setVisObjJ2(false);
 				}
-				
+
 				else if ( estSurObjectifJ1(piocheX, piocheY) && mot.getcurrentPlayer() == 0){
 					panneauDeJeu.setVisObjJ1(true);
 					panneauDeJeu.repaint();
 				}
-				
+
 				else if ( estSurObjectifJ2(piocheX, piocheY) && mot.getcurrentPlayer() == 1){
 					panneauDeJeu.setVisObjJ2(true);
 					panneauDeJeu.repaint();
@@ -80,35 +80,40 @@ public class Ecouteur_Plateau implements MouseListener, MouseMotionListener{
 					joueur.coupTourner(tuileNo(piocheX));
 					panneauDeJeu.repaint();
 				}
-				
+
 				// si le joueur selectionne une carte dans sa main
 				else if ( estDansMain(piocheX, piocheY) 
-					   && mot.getTabPlayers()[mainNo(piocheY)-1].getMain().getTuileAt(tuileNo(piocheX)) != null ){
-					
+						&& mot.getTabPlayers()[mainNo(piocheY)-1].getMain().getTuileAt(tuileNo(piocheX)) != null 
+						&& !mot.getTabPlayers()[mainNo(piocheY)-1].getVoyageActive() ){
+
 					int numeroTuile = tuileNo(piocheX);
 					int numeroMain = mainNo(piocheY);
-					
+
 					if ( mot.getcurrentPlayer() != numeroMain-1 ){
 						joueur.coupVoler(numeroTuile);
 					}
 					else {
 						panneauDeJeu.setPiocheSelectionnee(false);
 						panneauDeJeu.setCaseSelectionnee(false);
-						
+
 						// joueur.coupSelectionTuile(numeroTuile);
 					}
 					selectionnerMain(numeroTuile, numeroMain);
 				}
 				// si le joueur clique en dehors du plateau
 				else if ( estSurPlateau(caseX, caseY) ){
-					
+
 					panneauDeJeu.setPiocheSelectionnee(false);
 					// Pour voir si l'on a selectionne au prealable 
 					// la main du joueur
-					if (panneauDeJeu.getMainSelectionnee() == mot.getcurrentPlayer()+1){
+					if (panneauDeJeu.getMainSelectionnee() == mot.getcurrentPlayer()+1
+							&& !mot.getTabPlayers()[mot.getcurrentPlayer()].getVoyageActive() ){
 						joueur.coupPlacerTuile(panneauDeJeu.getTuileMainSelectionnee(), caseX, caseY);
-						
 						panneauDeJeu.setMainSelectionnee(-1);
+					}
+					else if (panneauDeJeu.getMainSelectionnee() == mot.getcurrentPlayer()+1
+							&& mot.getTabPlayers()[mot.getcurrentPlayer()].getVoyageActive() ){
+
 					}
 					else {
 						selectionnerCase(caseX, caseY);
@@ -127,104 +132,90 @@ public class Ecouteur_Plateau implements MouseListener, MouseMotionListener{
 			}
 		}
 	}
-	
+
 	public void mouseMoved(MouseEvent e) {}
-	
+
 	public void mouseReleased(MouseEvent e) {
-		// Coordonnees
-					int x = e.getX();
-					int y = e.getY();
-					
-					// on supprime le decalage de dessin par rapport a la bordure gauche.
-					int piocheX = x - panneauDeJeu.getDepart();
-					int piocheY = y;
-		
-		if ( estSurObjectifJ1(piocheX, piocheY) && mot.getcurrentPlayer()==0){
-			panneauDeJeu.setVisObjJ1(false);
-			panneauDeJeu.repaint();
-		}
-		
-		else if ( estSurObjectifJ2(piocheX, piocheY) && mot.getcurrentPlayer()==1){
-			panneauDeJeu.setVisObjJ2(false);
-			panneauDeJeu.repaint();
-		}
+		panneauDeJeu.setVisObjJ1(false);
+		panneauDeJeu.setVisObjJ2(false);
+		panneauDeJeu.repaint();
 	}
 	public void mouseClicked(MouseEvent e) {}
 	public void mouseEntered(MouseEvent e) {}
 	public void mouseExited(MouseEvent e) {}
 	public void mouseDragged(MouseEvent e) {}
-	
-	
+
+
 	/*
 	 * Methodes Private de EcouteurSouris_Plateau
 	 */
 	private boolean estSurPlateau(int caseX, int caseY) {
 		boolean clicValide = true;
-		
+
 		if ( caseX <= 0 || caseX > mot.getPlateau().length()-2 
-		||	 caseY <= 0 || caseY > mot.getPlateau().length()-2 ){
+				||	 caseY <= 0 || caseY > mot.getPlateau().length()-2 ){
 			clicValide = false;
 		}
 		return clicValide;
 	}
-	
+
 	private boolean estSurObjectifJ1(int Xget, int Yget){
 		boolean clicValideSurObj1 = false;
 		int borneGauche = panneauDeJeu.getPositionXObjJ1()-90;
 		int borneDroite = panneauDeJeu.getPositionXObjJ1() -90 + (panneauDeJeu.getTailleCase()*30)/10;
 		int borneHaute = panneauDeJeu.getPositionYObjJ1();
 		int borneBasse = panneauDeJeu.getPositionYObjJ1() + (panneauDeJeu.getTailleCase()*25)/10;
-		
+
 		if ( Xget >= borneGauche && Xget <= borneDroite
-		&&	 Yget >= borneHaute && Yget <= borneBasse ){
+				&&	 Yget >= borneHaute && Yget <= borneBasse ){
 			clicValideSurObj1 = true;
 		}
 		return clicValideSurObj1;
 	}
-	
+
 	private boolean estSurObjectifJ2(int Xget, int Yget){
 		boolean clicValideSurObj1 = false;
 		int borneGauche = panneauDeJeu.getPositionXObjJ2() - 90;
 		int borneDroite = panneauDeJeu.getPositionXObjJ2() -90 + (panneauDeJeu.getTailleCase()*30)/10;
 		int borneHaute = panneauDeJeu.getPositionYObjJ2();
 		int borneBasse = panneauDeJeu.getPositionYObjJ2() + (panneauDeJeu.getTailleCase()*25)/10;
-		
+
 		if ( Xget >= borneGauche && Xget <= borneDroite
-		&&	 Yget >= borneHaute && Yget <= borneBasse ){
+				&&	 Yget >= borneHaute && Yget <= borneBasse ){
 			clicValideSurObj1 = true;
 		}
 		return clicValideSurObj1;
 	}
-	
+
 	private boolean estSurPioche(int piocheX, int piocheY) {
 		boolean clicValideSurPioche = false;
 		int borneGauche = panneauDeJeu.getPositionXPioche() +10;
 		int borneDroite = borneGauche + panneauDeJeu.getDimensionPioche();
 		int borneHaute = panneauDeJeu.getPositionYPioche() +10;
 		int borneBasse = borneHaute + panneauDeJeu.getDimensionPioche();
-		
+
 		if ( piocheX >= borneGauche && piocheX <= borneDroite
-		&&	 piocheY >= borneHaute && piocheY <= borneBasse ){
+				&&	 piocheY >= borneHaute && piocheY <= borneBasse ){
 			clicValideSurPioche = true;
 		}
 		return clicValideSurPioche;
 	}
 	private boolean estDansMain(int piocheX, int piocheY) {
 		boolean clicValide = true;
-		
+
 		piocheX = piocheX + panneauDeJeu.getDepart();
-		
+
 		// indique dans quelle main le clic a ete effectue
 		int numeroMain = mainNo(piocheY);
 		int zoneDeLaMain = panneauDeJeu.getDecalageMain() + 4*( panneauDeJeu.getTailleCaseMain() + panneauDeJeu.getPetitEcartMain() ) + panneauDeJeu.getTailleCaseMain();
-		
+
 		if (piocheX >= panneauDeJeu.getDecalageMain() && piocheX <= zoneDeLaMain && numeroMain != -1){
 			int nombreEcarts = 4;
-			
+
 			for (int numeroEcart = 0; numeroEcart < nombreEcarts; numeroEcart++){
 				int borneGauche = panneauDeJeu.getDecalageMain() + panneauDeJeu.getTailleCaseMain() + numeroEcart * (panneauDeJeu.getTailleCaseMain() + panneauDeJeu.getPetitEcartMain());
 				int borneDroite = borneGauche + panneauDeJeu.getPetitEcartMain();
-				
+
 				if (piocheX > borneGauche && piocheX < borneDroite){
 					clicValide = false;
 				}
@@ -242,13 +233,13 @@ public class Ecouteur_Plateau implements MouseListener, MouseMotionListener{
 		int tailleCase = panneauDeJeu.getTailleCase();
 		int depart = panneauDeJeu.getDepart();
 		boolean b = false;
-		
+
 		if(mainSelected == 1) //Main basse
 		{
 			int main = panneauDeJeu.getMaintDuBas();
-			
+
 			if ( piocheX >= (coordX - tailleCase/3 + 10) - depart && piocheX <= (coordX-tailleCase/3 + 10 + tailleCase/2 - depart) 
-			  && piocheY >= main - tailleCaseM/3 + 10 && piocheY <= main - tailleCaseM/3 + 10 + tailleCaseM/2 )
+					&& piocheY >= main - tailleCaseM/3 + 10 && piocheY <= main - tailleCaseM/3 + 10 + tailleCaseM/2 )
 			{
 				b = true;
 			}
@@ -257,15 +248,15 @@ public class Ecouteur_Plateau implements MouseListener, MouseMotionListener{
 		{
 			int main = panneauDeJeu.getMainDuHaut();
 			if ( piocheX >= (coordX - tailleCase/3 + 10) - depart && piocheX <= ( coordX - tailleCase/3 + 10 + tailleCase/2 - depart) 
-			  && piocheY >= main - tailleCaseM/3 + 10 && piocheY <= main - tailleCaseM/3 + 10 + tailleCaseM/2 )
+					&& piocheY >= main - tailleCaseM/3 + 10 && piocheY <= main - tailleCaseM/3 + 10 + tailleCaseM/2 )
 			{
 				b = true;
 			}
 		}
-		
+
 		return b;
 	}
-	
+
 	private void selectionnerMain (int numeroTuile, int numeroMain){
 		panneauDeJeu.setMainSelectionnee(numeroMain);
 		panneauDeJeu.setTuileSelectionnee(numeroTuile);
@@ -280,7 +271,7 @@ public class Ecouteur_Plateau implements MouseListener, MouseMotionListener{
 		panneauDeJeu.setCoordYSelection(caseY);
 		panneauDeJeu.repaint();
 	}
-	
+
 	private int mainNo(int piocheY) {
 		int numeroMain;
 		if ( piocheY >= panneauDeJeu.getMaintDuBas() && piocheY <= panneauDeJeu.getMaintDuBas() + panneauDeJeu.getTailleCaseMain() ){
@@ -299,7 +290,7 @@ public class Ecouteur_Plateau implements MouseListener, MouseMotionListener{
 		// on definit les deux bornes d'une tuile
 		int borneGauche = panneauDeJeu.getDecalageMain();
 		int borneDroite = borneGauche + panneauDeJeu.getTailleCaseMain();
-		
+
 		for (int numeroTuile = 0; numeroTuile < 5; numeroTuile++){
 			// Si la position X se trouve entre les 2 bornes, on retourne le numero de la tuile
 			if ( piocheX >= borneGauche && piocheX <= borneDroite ){
