730902ae2a68225e4c95b1634aebb40930edd1ce
Piapiou <pierre-42@live.fr>
2015-06-08 12:44:03 +0200

    Amelioration IA ! (4 seconde d'execution)

--- StreetCar/src/iaPackage/IADifficile.java
+++ StreetCar/src/iaPackage/IADifficile.java
@@ -10,6 +10,7 @@ import joueurPackage.JoueurIA;
 import joueurPackage.MainJoueur;
 import mainPackage.Moteur;
 import objectPackage.Plateau;
+import objectPackage.tuilePackage.Escale;
 import objectPackage.tuilePackage.Tuile;
 import constantesPackages.Constantes;
 
@@ -28,7 +29,7 @@ public class IADifficile implements InterfaceIA {
 	public IADifficile(Moteur moteur, JoueurIA joueurIA) {
 		this.moteur = moteur;
 		joueur = joueurIA;
-		r = new Random();
+		r = new Random(3);
 		coupEnAttente = null;
 	}
 
@@ -58,7 +59,6 @@ public class IADifficile implements InterfaceIA {
 		Plateau plateau = (Plateau) moteur.getPlateau().clone();
 
 		MainJoueur main = joueur.getMain();
-		int evaluationInitiale = evaluationPlateau(plateau);
 
 		CoupEtRotation coupSoloActuel;
 		CoupEtRotation coupDuoActuel;
@@ -106,13 +106,15 @@ public class IADifficile implements InterfaceIA {
 				}
 			}
 		}
-		
-		for (int i = 0; i < 5; i++) {
-			System.out.print("TUILE N°"+(i+1)+" | UNIQUE : "+unique[i]+" | ROTATION : "); 
-			for (int r = 0; r < 4; r++){
-				System.out.print(uniqueRotation[i][r]+" "); 
+
+		if (trace == true) {
+			for (int i = 0; i < 5; i++) {
+				System.out.print("TUILE N°"+(i+1)+" | UNIQUE : "+unique[i]+" | ROTATION : "); 
+				for (int r = 0; r < 4; r++){
+					System.out.print(uniqueRotation[i][r]+" "); 
+				}
+				System.out.println(); 
 			}
-			System.out.println(); 
 		}
 
 		// PARTIE 1 : SIMULER TOUT LES COUP ET GARDER LE MEILLEUR DE TOUS
@@ -123,8 +125,9 @@ public class IADifficile implements InterfaceIA {
 					if (main.getTuileAt(numTuile) != null && unique[numTuile]){	
 						for (int nbRotation = 0; nbRotation < 4; nbRotation++) { 	// Et pour chaque orientation...
 							if (uniqueRotation[numTuile][nbRotation]) {
-								coupSoloActuel = new CoupEtRotation(new Coup(Constantes.Coup.placement, numTuile, new Point(x,y)), nbRotation);
+								coupSoloActuel = new CoupEtRotation(new Coup(Coup.placement, numTuile, new Point(x,y)), nbRotation);
 								if (plateau.coupValide(main.getTuileAt(coupSoloActuel.getCoup().getTuile()), coupSoloActuel.getCoup())) {
+									
 									jouerCoupProvisoire(plateau, x, y, numTuile, main);
 
 									coutActuel = evaluationPlateau(plateau);
@@ -138,7 +141,7 @@ public class IADifficile implements InterfaceIA {
 									}
 
 									if (moteur.getNumTour() > nbTourAvantIAPlusDevellope) {
-										coupDuoActuel = secondCoupAdjacent(plateau, coupSoloActuel, nbRotation);
+										coupDuoActuel = secondCoupAdjacent(plateau, coupSoloActuel);
 										if (coupDuoActuel.getCoup() != null) {
 
 											if (coutDuo >= coupDuoActuel.getCout()) {
@@ -179,10 +182,10 @@ public class IADifficile implements InterfaceIA {
 					if (main.getTuileAt(numTuile) != null){
 						for (int nbRotation = 0; nbRotation < 4; nbRotation++) { 	// Et pour chaque orientation...
 							if (uniqueRotation[numTuile][nbRotation]) {
-								coupDuoActuel = new CoupEtRotation(new Coup(Constantes.Coup.placement, numTuile, new Point(x,y)), nbRotation);
+								coupDuoActuel = new CoupEtRotation(new Coup(Coup.placement, numTuile, new Point(x,y)), nbRotation);
 
 								if (plateau.coupValide(main.getTuileAt(coupDuoActuel.getCoup().getTuile()), coupDuoActuel.getCoup())) {
-
+									
 									jouerCoupProvisoire(plateau, x, y, numTuile, main);
 									coupDuoActuel.setCout(evaluationPlateau(plateau));
 
@@ -234,9 +237,9 @@ public class IADifficile implements InterfaceIA {
 				&& !(cr1.getCoup().getCoordonnee().y <= cr2.getCoup().getCoordonnee().y+1 && cr1.getCoup().getCoordonnee().y >= cr2.getCoup().getCoordonnee().y-1 && cr1.getCoup().getCoordonnee().x == cr2.getCoup().getCoordonnee().x));
 	}
 
-	private CoupEtRotation secondCoupAdjacent(Plateau plateau, CoupEtRotation c, int evaluationInitiale) {
-		int coutActuel = evaluationInitiale;
-		//long t = System.currentTimeMillis();
+	private CoupEtRotation secondCoupAdjacent(Plateau plateau, CoupEtRotation c) {
+		int coutActuel = Integer.MAX_VALUE;
+		
 		CoupEtRotation coupGarde = new CoupEtRotation(null,0); coupGarde.setCout(Integer.MAX_VALUE/2);
 		CoupEtRotation coupCourant;
 		MainJoueur main = joueur.getMain();
@@ -245,19 +248,19 @@ public class IADifficile implements InterfaceIA {
 			for (int numTuile = 0; numTuile < 5; numTuile++) {
 				if (main.getTuileAt(numTuile) != null) {
 					for (int nbRotation = 0; nbRotation < 4; nbRotation++) { 	
-						switch (i) {
-						case 0 : 
-							coupCourant = new CoupEtRotation(Coup.newPlacement(numTuile, x+1, y),nbRotation); break;
-						case 1 :
-							coupCourant = new CoupEtRotation(Coup.newPlacement(numTuile, x-1, y),nbRotation); break;
-						case 2 :
-							coupCourant = new CoupEtRotation(Coup.newPlacement(numTuile, x, y+1),nbRotation); break;
-						default :
-							coupCourant = new CoupEtRotation(Coup.newPlacement(numTuile, x, y-1),nbRotation); break;		
-						}
+						Coup c1;
+						if (i == 0)
+							c1 = Coup.newPlacement(numTuile, x+1, y);
+						else if (i == 0) 
+							c1 = Coup.newPlacement(numTuile, x-1, y);
+						else if (i == 0) 
+							c1 = Coup.newPlacement(numTuile, x, y+1);
+						else
+							c1 = Coup.newPlacement(numTuile, x, y-1);
+						coupCourant = new CoupEtRotation(c1,nbRotation);
 						if (plateau.coupValide(main.getTuileAt(numTuile), coupCourant.getCoup())) {
 							jouerCoupProvisoire(plateau, x, y, numTuile, main);
-							coupCourant.setCout(evaluationPlateau(plateau)-evaluationInitiale);
+							coupCourant.setCout(evaluationPlateau(plateau));
 							if (coupCourant.getCout() < coutActuel) {
 								coupGarde = coupCourant;
 								coutActuel = coupCourant.getCout();
@@ -269,7 +272,6 @@ public class IADifficile implements InterfaceIA {
 				}
 			}
 		}
-		//System.out.println("Temps d'execution coup duo (ms) : "+(System.currentTimeMillis()-t));
 		return coupGarde;
 	}
 
@@ -280,13 +282,11 @@ public class IADifficile implements InterfaceIA {
 	}
 
 	private int evaluationPlateau(Plateau p) {
-		//long t = System.nanoTime()/1000;
+		long t = System.nanoTime()/1000;
 		int c1 = coutCheminBis(joueur.getObjectifs().getLigne(),joueur.getObjectifs().getEscalesCibles(),p);
 		int c2 = coutCheminBis(moteur.getTabPlayers()[(moteur.getcurrentPlayer()+1)%2].getObjectifs().getLigne(),moteur.getTabPlayers()[(moteur.getcurrentPlayer()+1)%2].getObjectifs().getEscalesCibles(),p);
 
-
-		//if (trace == true) 
-		//	System.out.println("Temps d'execution 2 recherche (µs) : "+(System.nanoTime()/1000-t));
+		//System.out.println("Temps d'execution 2 recherche (µs) : "+(System.nanoTime()/1000-t));
 		if (c1 == 0) {
 			return Integer.MIN_VALUE/4;
 		}
@@ -298,7 +298,7 @@ public class IADifficile implements InterfaceIA {
 	private final static int coutTuilePossible = 5;
 	private final static int coutHeuristique = 1;
 
-	private int coutCheminBis(int ligne, int[] escales, Plateau p) {
+	private int coutChemin(int ligne, int[] escales, Plateau p) {
 		int[] escalesBis = new int[3];
 		for (int i = 0; i< 3; i++)
 			escalesBis[i] = escales[i];
@@ -309,6 +309,7 @@ public class IADifficile implements InterfaceIA {
 
 	private int rechercheChemin(Point depart, String directionDepart, Point arrivee, int[] escales, Plateau plateau, int coutInitial) {
 
+		long time = System.nanoTime();
 		PriorityQueue<TuileChemin> file = new PriorityQueue<TuileChemin>
 		(Constantes.Dimensions.dimensionPlateau*Constantes.Dimensions.dimensionPlateau, new ComparateurChemin());
 		Point po = depart;
@@ -464,6 +465,7 @@ public class IADifficile implements InterfaceIA {
 			lastOne = lastOne.getPrevious();
 		}
 		System.out.println("---------");*/
+		//System.out.println(System.nanoTime()-time);
 
 		return coutFinal;
 	}
@@ -480,4 +482,201 @@ public class IADifficile implements InterfaceIA {
 		return i;
 	}
 
+	private int max(int i, int j) {
+		if (i < j)
+			return j;
+		return i;
+	}
+
+
+
+	private int coutCheminBis(int ligne, int[] escales, Plateau p) {
+		int[] escalesBis = new int[3];
+		for (int i = 0; i< 3; i++)
+			escalesBis[i] = escales[i];
+
+		escalesBis[0] = -1;
+		int x = rechercheCheminBis(p.getTerminalPosition(ligne,1),Constantes.Orientation.ouest,p.getEscalelPosition(escales[0]), ligne, escalesBis, p, 0);
+		escalesBis[0] = escales[0];
+
+		escalesBis[1] = -1;
+		int y = rechercheCheminBis(p.getTerminalPosition(ligne,1),Constantes.Orientation.ouest,p.getEscalelPosition(escales[1]), ligne, escalesBis, p, 0);
+		escalesBis[1] = escales[1];
+
+		return max(x,y);
+	}
+
+	private int rechercheCheminBis(Point depart, String directionDepart, Point arrivee, int ligne, int[] escales, Plateau plateau, int coutInitial) {
+
+		// Initialisation
+		long time = System.nanoTime();
+		PriorityQueue<TuileChemin> file = new PriorityQueue<TuileChemin>
+		(Constantes.Dimensions.dimensionPlateau*Constantes.Dimensions.dimensionPlateau, new ComparateurChemin());
+		Point po = depart;
+		TuileChemin tuileCheminCourant = new TuileChemin(po,directionDepart,coutInitial,0, null);
+		file.add(tuileCheminCourant);
+		po = new Point();
+		RejectList seens = new RejectList();
+		boolean found = false;
+		int coutFinal = Integer.MAX_VALUE;
+		int escaleAdjacente;
+		
+		// Verifier si l'objectif actuel est une Escale
+		Tuile tuileArrivee = plateau.getTuileAt(arrivee.x, arrivee.y);
+		int escaleCourante = 0; 
+		if (tuileArrivee instanceof Escale) {
+			Escale e = (Escale) tuileArrivee;
+			escaleCourante = e.getNumeroEscale();
+		}
+
+		// Debut du A*
+		do {
+			// Récupérer la tuile suivante
+			tuileCheminCourant = file.remove();
+			Point pCourant = tuileCheminCourant.getPosition();
+			Tuile t = plateau.getPlateau()[pCourant.x][pCourant.y];
+
+			
+			if (!seens.contain(tuileCheminCourant)) {
+				seens.add(tuileCheminCourant);
+
+				escaleAdjacente = plateau.aCoterDUneEscaleNonAssignee(pCourant);
+
+				// Cas de la tuile null
+				if (t == null) {
+					if (!tuileCheminCourant.getDirection().equals(Constantes.Orientation.est)) {
+						po.x = pCourant.x+1; po.y = pCourant.y;
+						file.add(new TuileChemin(po,Constantes.Orientation.ouest,tuileCheminCourant.getPriority()+coutTuileNull,calculHeuristique(po, arrivee, false), tuileCheminCourant));
+					}
+					if (!tuileCheminCourant.getDirection().equals(Constantes.Orientation.ouest)){
+						po.x = pCourant.x-1; po.y = pCourant.y;
+						file.add(new TuileChemin(po,Constantes.Orientation.est,tuileCheminCourant.getPriority()+coutTuileNull,calculHeuristique(po, arrivee, false), tuileCheminCourant));
+					}
+					if (!tuileCheminCourant.getDirection().equals(Constantes.Orientation.sud)) {
+						po.x = pCourant.x; po.y = pCourant.y+1;
+						file.add(new TuileChemin(po,Constantes.Orientation.nord,tuileCheminCourant.getPriority()+coutTuileNull,calculHeuristique(po, arrivee, false), tuileCheminCourant));
+					}
+					if (!tuileCheminCourant.getDirection().equals(Constantes.Orientation.nord)) {
+						po.x = pCourant.x; po.y = pCourant.y-1;
+						file.add(new TuileChemin(po,Constantes.Orientation.sud,tuileCheminCourant.getPriority()+coutTuileNull,calculHeuristique(po, arrivee, false), tuileCheminCourant));
+					}
+				}
+
+				// Cas de la tuile d'escale de début;
+				else if (t.getTypeTuile() == 1 && pCourant.equals(depart)) {
+					if (t.connectionsExistantes(Constantes.Orientation.est)) {
+						po.x = pCourant.x+1; po.y = pCourant.y;
+						file.add(new TuileChemin(po,Constantes.Orientation.ouest,tuileCheminCourant.getPriority(),calculHeuristique(po, arrivee, false), tuileCheminCourant));
+					}
+					if (t.connectionsExistantes(Constantes.Orientation.ouest)) {
+						po.x = pCourant.x-1; po.y = pCourant.y;
+						file.add(new TuileChemin(po,Constantes.Orientation.est,tuileCheminCourant.getPriority(),calculHeuristique(po, arrivee, false), tuileCheminCourant));
+					}
+					if (t.connectionsExistantes(Constantes.Orientation.sud)) {
+						po.x = pCourant.x; po.y = pCourant.y+1;
+						file.add(new TuileChemin(po,Constantes.Orientation.nord,tuileCheminCourant.getPriority(),calculHeuristique(po, arrivee, false), tuileCheminCourant));
+					}
+					if (t.connectionsExistantes(Constantes.Orientation.nord)) {
+						po.x = pCourant.x; po.y = pCourant.y-1;
+						file.add(new TuileChemin(po,Constantes.Orientation.sud,tuileCheminCourant.getPriority(),calculHeuristique(po, arrivee, false), tuileCheminCourant));
+					}
+				}
+
+				// Cas des tuiles avec présence d'arbre.
+				else if (t.getPresenceArbres()) {
+					for (String s : t.getDirectionConnectedTo(tuileCheminCourant.getDirection())) {
+						switch (s) {
+						case Constantes.Orientation.est : 
+							po.x = pCourant.x+1; po.y = pCourant.y;
+							file.add(new TuileChemin(po,Constantes.Orientation.ouest,tuileCheminCourant.getPriority()+coutTuileFixe,calculHeuristique(po, arrivee, false), tuileCheminCourant));
+							break;
+						case Constantes.Orientation.ouest : 
+							po.x = pCourant.x-1; po.y = pCourant.y;
+							file.add(new TuileChemin(po,Constantes.Orientation.est,tuileCheminCourant.getPriority()+coutTuileFixe,calculHeuristique(po, arrivee, false), tuileCheminCourant));
+							break;
+						case Constantes.Orientation.sud : 
+							po.x = pCourant.x; po.y = pCourant.y+1;
+							file.add(new TuileChemin(po,Constantes.Orientation.nord,tuileCheminCourant.getPriority()+coutTuileFixe,calculHeuristique(po, arrivee, false), tuileCheminCourant));
+							break;
+						case Constantes.Orientation.nord: 
+							po.x = pCourant.x; po.y = pCourant.y-1;
+							file.add(new TuileChemin(po,Constantes.Orientation.sud,tuileCheminCourant.getPriority()+coutTuileFixe,calculHeuristique(po, arrivee, false), tuileCheminCourant));
+							break;
+						}
+					}
+				}
+
+				// Cas de base
+				else {
+					ArrayList<String> listeDirectionPossible = t.getDirectionConnectedTo(tuileCheminCourant.getDirection());
+					if (!tuileCheminCourant.getDirection().equals(Constantes.Orientation.est)) {
+						po.x = pCourant.x+1; po.y = pCourant.y;
+						if (listeDirectionPossible.contains(Constantes.Orientation.est))
+							file.add(new TuileChemin(po,Constantes.Orientation.ouest,tuileCheminCourant.getPriority()+coutTuileFixe,calculHeuristique(po, arrivee, false), tuileCheminCourant));
+						else
+							file.add(new TuileChemin(po,Constantes.Orientation.ouest,tuileCheminCourant.getPriority()+coutTuilePossible,calculHeuristique(po, arrivee, false), tuileCheminCourant));
+					}
+					if (!tuileCheminCourant.getDirection().equals(Constantes.Orientation.ouest)) {
+						po.x = pCourant.x-1; po.y = pCourant.y;
+						if (listeDirectionPossible.contains(Constantes.Orientation.ouest))
+							file.add(new TuileChemin(po,Constantes.Orientation.est,tuileCheminCourant.getPriority()+coutTuileFixe,calculHeuristique(po, arrivee, false), tuileCheminCourant));
+						else
+							file.add(new TuileChemin(po,Constantes.Orientation.est,tuileCheminCourant.getPriority()+coutTuilePossible,calculHeuristique(po, arrivee, false), tuileCheminCourant));
+					}
+					if (!tuileCheminCourant.getDirection().equals(Constantes.Orientation.sud)) {
+						po.x = pCourant.x; po.y = pCourant.y+1;
+						if (listeDirectionPossible.contains(Constantes.Orientation.sud))
+							file.add(new TuileChemin(po,Constantes.Orientation.nord,tuileCheminCourant.getPriority()+coutTuileFixe,calculHeuristique(po, arrivee, false), tuileCheminCourant));
+						else
+							file.add(new TuileChemin(po,Constantes.Orientation.nord,tuileCheminCourant.getPriority()+coutTuilePossible,calculHeuristique(po, arrivee, false), tuileCheminCourant));
+					}
+					if (!tuileCheminCourant.getDirection().equals(Constantes.Orientation.nord)) {
+						po.x = pCourant.x; po.y = pCourant.y-1;
+						if (listeDirectionPossible.contains(Constantes.Orientation.nord))
+							file.add(new TuileChemin(po,Constantes.Orientation.sud,tuileCheminCourant.getPriority()+coutTuileFixe,calculHeuristique(po, arrivee, false), tuileCheminCourant));
+						else
+							file.add(new TuileChemin(po,Constantes.Orientation.sud,tuileCheminCourant.getPriority()+coutTuilePossible,calculHeuristique(po, arrivee, false), tuileCheminCourant));
+					}
+				}
+
+				if (escaleCourante == 0 && pCourant.equals(arrivee) ) {
+					coutFinal = tuileCheminCourant.getPriority();
+					found = true;
+				} else if (escaleCourante != 0 && (escaleAdjacente == escaleCourante || (t != null && t.getEscaleLiee() == escaleCourante))) {
+					int i = 0;
+					while (i < 3 && !found) {
+						if (escales[i] != -1) {
+							int e = escales[i];
+							//System.out.println(e +" "+ escales[i] + " " + escaleCourante + " " + escaleAdjacente);
+							escales[i] = -1;
+							coutFinal = rechercheCheminBis(pCourant, tuileCheminCourant.getDirection(), plateau.getEscalelPosition(e), ligne, escales, plateau, tuileCheminCourant.getPriority());
+							found = true;
+							escales[i] = e;
+						}
+						i++;
+						//p.getTerminalPosition(ligne,2)
+					}
+					if (!found) {
+						coutFinal = rechercheCheminBis(pCourant, tuileCheminCourant.getDirection(), plateau.getTerminalPosition(ligne,2), ligne, escales, plateau, tuileCheminCourant.getPriority());
+						found = true;
+					}
+				}
+
+			}
+
+		} while(!found && !file.isEmpty());
+
+		/*TuileChemin lastOne = tuileCheminCourant;
+		while(lastOne != null) {
+			System.out.println(lastOne.getPosition()+" "+lastOne.getDirection()+" | cout :"+lastOne.getPriority());
+			lastOne = lastOne.getPrevious();
+		}
+		System.out.println("---------");*/
+		//System.out.println(System.nanoTime()-time);
+
+		return coutFinal;
+	}
+
 }
+
+
